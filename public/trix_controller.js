26d65dffd436ba786da9d307de79e56b17ab5f76
Omer Sadikovic <omer.sadikovic2001@gmail.com>
2023-10-03 17:19:18 +0200

    optButton

--- public/trix_controller.js
+++ public/trix_controller.js
@@ -2,14 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
     const optionsButton = document.getElementById("optionsButton");
     const optionsMenu = document.getElementById("optionsMenu");
 
-    optionsButton.addEventListener("click", function () {
-        // Toggle the visibility of the options menu
-        if (optionsMenu.style.display === "block") {
-            optionsMenu.style.display = "none";
-        } else {
-            optionsMenu.style.display = "block";
-        }
-    });
+    if(optionsButton){
+
+        optionsButton.addEventListener("click", function () {
+            // Toggle the visibility of the options menu
+            if (optionsMenu.style.display === "block") {
+                optionsMenu.style.display = "none";
+            } else {
+                optionsMenu.style.display = "block";
+            }
+        });
+    }
 
     // Close the dropdown menu if the user clicks outside of it
     window.addEventListener("click", function (event) {
